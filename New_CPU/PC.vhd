194305bdc1a4480303718e43661a7484c76de433
David Debreceni Jr <davidjr@debreceni.net>
2017-10-03 23:27:15 -0400

    Implemented Program Counter

--- New_CPU/PC.vhd
+++ New_CPU/PC.vhd
@@ -5,10 +5,24 @@
 --------------------------------------------
 
 library IEEE;
-use ieee.std_logic_1164.all
+use ieee.std_logic_1164.all;
 
 entity PC is
 port(clk:in std_logic;
 	AddressIn:in std_logic_vector(31 downto 0);
 	AddressOut:out std_logic_vector(31 downto 0));
 end PC;
+
+architecture behavioral of PC is
+signal temp: std_logic_vector(31 downto 0) := "00000000000000000000000000000000";
+begin
+
+process(clk)
+begin
+	if clk='1' and clk'event then
+		temp <= AddressIn;
+	end if;
+	AddressOut <= temp;
+end process;
+
+end behavioral;
