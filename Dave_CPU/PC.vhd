0da1ebc0cc02ed219443a472022071e410a03702
anush95 <anush95@gmail.com>
2017-10-25 23:22:30 -0400

    removed file

--- Dave_CPU/PC.vhd
+++ Dave_CPU/PC.vhd
@@ -1,4 +1,4 @@
-library IEEE;
+	library IEEE;
 use ieee.std_logic_1164.all;
 
 entity PC is
@@ -12,14 +12,19 @@ signal temp: std_logic_vector(31 downto 0) := "00000000000000000000000000000000"
 begin
 
 
-process(clk)
-begin
-if clk = '1' and clk'event then
-temp <= AddressIn;
-end if;
-AddressOut <= temp;
-end process;
 
-
+	process(clk)
+	variable count : integer := 0;
+	begin
+		if clk='1' and clk'event then
+			--temp <= AddressIn;
+			if count=0 then
+				AddressOut <= (others => '0');
+				count := count + 1;
+			else
+				AddressOut <= AddressIn;
+			end if;
+		end if;
+	end process;
 
 end behavioral;
