7120edf6794389080ed4179d377362e9f4c33df8
anush95 <anush95@gmail.com>
2017-10-31 16:28:04 -0400

    Error in control naming

--- Multicycle/Multicycle_control.vhd
+++ Multicycle/Multicycle_control.vhd
@@ -3,66 +3,66 @@ use IEEE.STD_LOGIC_1164.ALL;
 
 
 
-entity MulticycleControl_output is
+entity MulticycleControl is
 port(Opcode:in std_logic_vector(5 downto 0);
      clk:in std_logic;
      RegDst,RegWrite,ALUSrcA,IRWrite,MemtoReg,MemWrite,MemRead,IorD,PCWrite,PCWriteCond:out std_logic;
      ALUSrcB,ALUOp,PCSource:out std_logic_vector(1 downto 0));
-end MulticycleControl_output;
+end MulticycleControl;
 
-architecture behave of MulticycleControl_output is
+architecture behave of MulticycleControl is
 begin
 
 process(clk, Opcode)
 type state is (state0,state1,state2,state3,state4,state5,state6,state7,state8,state9);
-variable n_s : state := state0;
+variable next_state : state := state0;
 variable Control_output:std_logic_vector(15 downto 0);
 begin
 
 if clk='1' and clk'event then
-    case n_s is
+    case next_state is
         when state0 => 
 	    Control_output := "UU01UU101U010000";
-            n_s := state1;
+            next_state := state1;
         when state1 =>
             Control_output := "UU0UUUUUUU1100UU";
             if (Opcode="100011") or (Opcode="101011") then
-                n_s := state2; -- LW or SW
+                next_state := state2; -- LW or SW
             elsif Opcode="000000" then
-                n_s := state6; -- R-Type
+                next_state := state6; -- R-Type
             elsif Opcode="000100" then
-                n_s := state8; -- BEQ
+                next_state := state8; -- BEQ
             elsif Opcode="000010" then
-                n_s := state9; -- Jump
+                next_state := state9; -- Jump
             end if;
         when state2 =>
             Control_output := "UU1UUUUUUU1000UU";
             if Opcode="100011" then
-                n_s := state3; -- LW
+                next_state := state3; -- LW
             elsif Opcode="101011" then
-                n_s := state5; -- SW
+                next_state := state5; -- SW
             end if;
         when state3 =>
             Control_output := "UUUUUU11UUUUUUUU";
-            n_s := state4;
+            next_state := state4;
         when state4 =>
             Control_output := "01UU1UUUUUUUUUUU";
-            n_s := state0;
+            next_state := state0;
         when state5 =>
             Control_output := "UUUUU1U1UUUUUUUU";
-            n_s := state0;
+            next_state := state0;
         when state6 =>
             Control_output := "UU1UUUUUUU0010UU";
-            n_s := state7;
+            next_state := state7;
         when state7 =>
             Control_output := "11UU0UUUUUUUUUUU";
-            n_s := state0;
+            next_state := state0;
         when state8 =>
             Control_output := "UU1UUUUUU1000101";
-            n_s := state0;
+            next_state := state0;
         when state9 =>
             Control_output := "UUUUUUUU1UUUUU10";
-            n_s := state0;
+            next_state := state0;
         when others =>
             null;
     end case;
