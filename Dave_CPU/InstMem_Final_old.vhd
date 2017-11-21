a121bb389195ac05bb32cc5a3000d687008f9933
Dave Debreceni <davidjr@debreceni.net>
2017-11-21 13:22:19 -0500

    Pulling from old, not sure why

--- Dave_CPU/InstMem_Final_old.vhd
+++ Dave_CPU/InstMem_Final_old.vhd
@@ -1,57 +1,39 @@
 library ieee;
 use ieee.std_logic_1164.all;
-use ieee.std_logic_arith.all;
-use ieee.std_logic_unsigned.all;
+use ieee.numeric_std.all;
 
 entity InstMemory is
 port(Address:in std_logic_vector(31 downto 0);  
-     ReadData:out std_logic_vector(31 downto 0);
-	ck:in std_logic);
+     ReadData:out std_logic_vector(31 downto 0));
 end InstMemory;
 
 architecture beh of InstMemory is
 type memoryArray is array(0 to 31) of std_logic_vector(31 downto 0);
 signal memContents:memoryArray := (
-    X"8D150000",
-    X"8D160004", 
-    X"02B6782A",
-    X"11E00002",
-    X"02538822",
-    X"08000007",
-    X"AD11000C",
-    X"00000000",
-    X"00000000",
-    X"00000000", 
-    X"00000000",
-    X"00000000", 
-    X"00000000",
-    X"00000000",
-    X"00000000",
-    X"00000000",
-    X"00000000",
-    X"00000000",
-    X"00000000",
-    X"00000000",  
-    X"00000000",
-    X"00000000",
-    X"00000000",
-    X"00000000",
-    X"00000000", 
-    X"00000000",
-    X"00000000",
-    X"00000000",
-    X"00000000",
-    X"00000000", 
-    X"00000000",
-    X"00000000");
+	-- Code 1
+	0 => X"02959820",
+	1 => X"8d100000",
+	2 => X"8d110004",
+	3 => X"0296b822",
+	4 => X"ad130008",
+	others => X"00000000"
 
-begin
-process(ck)
-begin
-if ck='0' and ck'event then
-	ReadData <= memContents(conv_integer(Address(31 downto 2)));
-end if;
---wait on address;       
-end process;
+	-- Code 2
 
+--	others => X"00000000"
+
+	-- Code 3
+
+--	others => X"00000000"
+
+	-- Code 4
+
+--	others => X"00000000"
+	
+	-- Code 5
+
+--	others => X"00000000"
+);
+begin
+ReadData <= memContents(to_integer(unsigned(Address))/4);
 end beh;
