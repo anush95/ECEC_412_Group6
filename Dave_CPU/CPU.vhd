46c9dd19d16edcd2b7bcf15d64bb3030507863e4
Dave Debreceni <davidjr@debreceni.net>
2017-11-21 13:18:02 -0500

    Implemented Pipeline Registers

--- Dave_CPU/CPU.vhd
+++ Dave_CPU/CPU.vhd
@@ -47,22 +47,17 @@ end component;
 
 component Control is
 port(Opcode: in std_logic_vector(5 downto 0);
-     RegDst, Branch, MemRead, MemtoReg, MemWrite, ALUSrc, RegWrite, Jump: out std_logic;
+     RegDst, Branch, MemRead, MemtoReg, MemWrite, ALUSrc, RegWrite: out std_logic;
      ALUOp: out std_logic_vector(1 downto 0));
 end component;
 
 component IDEXPR is 
-port(CLK:in std_logic;
-     WBCTRLIN:in std_logic_vector(1 downto 0);
-     MCTRLIN:in std_logic_vector(2 downto 0);
-     EXCTRLIN:in std_logic_vector(3 downto 0);
-     LIN,CIN,DIN,EIN:in std_logic_vector(31 downto 0);
-     RTIN,RDIN:in std_logic_vector(4 downto 0);
-     WBCTRLOUT:out std_logic_vector(1 downto 0);
-     MCTRLOUT:out std_logic_vector(2 downto 0);
-     EXCTRLOUT:out std_logic_vector(3 downto 0);
-     LOUT,COUT,DOUT,EOUT:out std_logic_vector(31 downto 0);
-     RTOUT,RDOUT:out std_logic_vector(4 downto 0));
+port(clk, RegDst_in, ALUSrc_in, Branch_in, MemRead_in, MemWrite_in, RegWrite_in, MemToReg_in :in std_logic;
+     ALUOp_in: in std_logic_vector(1 downto 0);
+     Address_in, Instruction_in, ReadData1_in, ReadData2_in:in std_logic_vector(31 downto 0);
+     RegDst_out, ALUSrc_out, Branch_out, MemRead_out, MemWrite_out, RegWrite_out, MemToReg_out: out std_logic;
+     ALUOp_out: out std_logic_vector(1 downto 0);
+     Address_out, Instruction_out, ReadData1_out, ReadData2_out: out std_logic_vector(31 downto 0));
 end component;
 
 -- EX/MEM
@@ -89,19 +84,12 @@ port(x,y: in std_logic_vector(4 downto 0);
 end component;
 
 component EXMEMPR is
-port(CLK:in std_logic;
-     WBCTRLIN:in std_logic_vector(1 downto 0);
-     MCTRLIN: in std_logic_vector(2 downto 0);
-     MIN:in std_logic_vector(31 downto 0);
-     ZIN:in std_logic;
-     GIN,DIN:in std_logic_vector(31 downto 0);
-     BIN:in std_logic_vector(4 downto 0);
-     WBCTRLOUT:out std_logic_vector(1 downto 0);
-     MCTRLOUT: out std_logic_vector(2 downto 0);
-     MOUT:out std_logic_vector(31 downto 0);
-     ZOUT:out std_logic;
-     GOUT,DOUT:out std_logic_vector(31 downto 0);
-     BOUT:out std_logic_vector(4 downto 0));
+port(clk, RegWrite_in, MemToReg_in, Branch_in, MemRead_in, MemWrite_in, Zero_in:in std_logic;
+	WriteReg_in: in std_logic_vector(4 downto 0);
+   	Address_in, ALURes_in, WriteData_in: in std_logic_vector(31 downto 0);
+	RegWrite_out, MemToReg_out, Branch_out, MemRead_out, MemWrite_out, Zero_out: out std_logic;
+	WriteReg_out: out std_logic_vector(4 downto 0);
+	Address_out, ALURes_out, WriteData_out: out std_logic_vector(31 downto 0));
 end component;
 
 -- MEM/WB
@@ -114,13 +102,12 @@ port(WriteData: in std_logic_vector(31 downto 0);
 end component;
 
 component MEMWBPR is
-port(CLK: in std_logic;
-	WBCTRLIN:in std_logic_vector(1 downto 0);
-	HIN,DIN:in std_logic_vector(31 downto 0);
-	BIN:in std_logic_vector(4 downto 0);
-	WBCTRLOUT:out std_logic_vector(1 downto 0);
-	HOUT,DOUT:out std_logic_vector(31 downto 0);
-	BOUT:out std_logic_vector(4 downto 0));
+port(clk, RegWrite_in, MemToReg_in: in std_logic;
+	WriteReg_in: std_logic_vector(4 downto 0);
+	ReadData_in, ALURes_in: in std_logic_vector(31 downto 0);
+	RegWrite_out, MemToReg_out: out std_logic;
+	WriteReg_out: out std_logic_vector(4 downto 0);
+	ReadData_out, ALURes_out: out std_logic_vector(31 downto 0));
 end component;
 
 component ShiftLeftTwo is
@@ -128,6 +115,55 @@ port(x: in std_logic_vector(31 downto 0);
      y: out std_logic_vector(31 downto 0));
 end component;
 
-begin
+  -- Signals
+  -- IF
+  signal AddressIF, InstructionIF, PCIn, PCOut: std_logic_vector(31 downto 0) := X"00000000";
+  -- ID
+  signal ALUSrcID, BranchID, MemReadID, MemWriteID, MemtoRegID, RegDstID, RegWriteID: std_logic := '0';
+  signal ALUOpID: std_logic_vector(1 downto 0) := "00";
+  signal AddressID, InstructionID, ReadDataOneID, ReadDataTwoID: std_logic_vector(31 downto 0) := X"00000000";
+  -- EX
+  signal ALUSrcEX, BranchEX, MemReadEX, MemWriteEX, MemtoRegEX, RegDstEX, RegWriteEX, ZeroEX: std_logic := '0';
+  signal ALUOpEX: std_logic_vector(1 downto 0) := "00";
+  signal Operation: std_logic_vector(3 downto 0) := "0000";
+  signal WriteRegisterEX: std_logic_vector(4 downto 0) := "00000";
+  signal AddressEX, ALUOperand, ALUResultEX, InstructionEX, MaskedInstruction, ReadDataOneEX, ReadDataTwoEX: std_logic_vector(31 downto 0) := X"00000000";
+  -- MEM
+  signal PCSrc, BranchMEM, MemWriteMEM, MemReadMEM, MemtoRegMEM, RegWriteMEM, ZeroMEM: std_logic := '0';
+  signal WriteRegisterMEM: std_logic_vector(4 downto 0) := "00000";
+  signal AddressMEM, ALUResultMEM, BranchInstruction, ReadDataMEM, ShiftedInstruction, WriteDataMEM: std_logic_vector(31 downto 0) := X"00000000";
+  -- WB
+  signal MemtoRegWB, RegWriteWB: std_logic := '0';
+  signal WriteRegisterWB: std_logic_vector(4 downto 0) := "00000";
+  signal ALUResultWB, ReadDataWB, WriteRegisterData: std_logic_vector(31 downto 0) := X"00000000";
 
+begin
+  -- Components
+  -- IF
+  Mux32_0: Mux32 port map(AddressIF, AddressMEM, PCSrc, PCIn);
+  PC_inst: PC port map(clk, PCIn, PCOut);
+  Add_0: Adder port map(PCOut, X"00000004", AddressIF);
+  InstructionMemory: InstMemory port map(PCOut, InstructionIF);
+  IFIDRegister: IFIDPR port map(clk, AddressIF, InstructionIF, AddressID, InstructionID);
+  -- ID
+  Registers_inst: registers port map(InstructionID(25 downto 21), InstructionID(20 downto 16), WriteRegisterWB, WriteRegisterData, RegWriteWB, clk, ReadDataOneID, ReadDataTwoID);
+  Control_inst: Control port map(InstructionID(31 downto 26), RegDstID, BranchID, MemReadID, MemtoRegID, MemWriteID, ALUSrcID, RegWriteID, ALUOpID);  
+  IDEXRegister: IDEXPR port map(clk, RegDstID, ALUSrcID, BranchID, MemReadID, MemWriteID, RegWriteID, MemtoRegID, ALUOpID, AddressID, InstructionID, ReadDataOneID, ReadDataTwoID,
+				     RegDstEX, ALUSrcEX, BranchEX, MemReadEX, MemWriteEX, RegWriteEX, MemtoRegEX, ALUOpEX, AddressEX, InstructionEX, ReadDataOneEX, ReadDataTwoEX);
+  -- EX
+  ALUControl_inst: ALUControl port map(ALUOpEX, InstructionEX(5 downto 0), Operation);
+  MaskedInstruction <= InstructionEX and X"0000FFFF";
+  ShiftLeft2: ShiftLeftTwo port map(MaskedInstruction, ShiftedInstruction);
+  Add_1: Adder port map(AddressEX, ShiftedInstruction, BranchInstruction);
+  Mux32_1: Mux32 port map(ReadDataTwoEX, MaskedInstruction, ALUSrcEX, ALUOperand);
+  ALU_inst: ALU32 port map(ReadDataOneEX, ALUOperand, Operation, ALUResultEX, ZeroEX);
+  Mux5_inst: Mux5 port map(InstructionEX(20 downto 16), InstructionEX(15 downto 11), RegDstEX, WriteRegisterEX);
+  EXMEMRegister: EXMEMPR port map(clk, RegWriteEX, MemtoRegEX, BranchEX, MemReadEX, MemWriteEx, ZeroEX, WriteRegisterEX, AddressEx, ALUResultEX, ReadDataTwoEX,
+				       RegWriteMEM, MemtoRegMEM, BranchMEM, MemReadMEM, MemWriteMEM, ZeroMEM, WriteRegisterMEM, AddressMEM, ALUResultMEM, WriteDataMEM);
+  -- MEM
+  PCSrc <= BranchMEM and ZeroMEM;
+  DataMemory: MemoryData port map(WriteDataMEM, ALUResultMEM, MemReadMEM, MemWriteMEM, ReadDataMEM);
+  MEMWBRegister: MEMWBPR port map(clk, RegWriteMEM, MemtoRegMEM, WriteRegisterMEM, ReadDataMEM, ALUResultMEM, RegWriteWB, MemToRegWB, WriteRegisterWB, ReadDataWB, ALUResultWB);
+  -- WB
+  Mux32_2: Mux32 port map(ALUResultWB, ReadDataWB, MemtoRegWB, WriteRegisterData);
 end struc;
