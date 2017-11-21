c4d12f93b7bbf25b7608c5a1880cc85aab499237
Dave Debreceni <davidjr@debreceni.net>
2017-11-21 13:14:33 -0500

    Modified MEMWBPR

--- Dave_CPU/MEMWBPR.vhd
+++ Dave_CPU/MEMWBPR.vhd
@@ -2,33 +2,32 @@ library ieee;
 use ieee.std_logic_1164.all;
 
 entity MEMWBPR is
-port(CLK: in std_logic;
-	WBCTRLIN:in std_logic_vector(1 downto 0);
-	HIN,DIN:in std_logic_vector(31 downto 0);
-	BIN:in std_logic_vector(4 downto 0);
-	WBCTRLOUT:out std_logic_vector(1 downto 0);
-	HOUT,DOUT:out std_logic_vector(31 downto 0);
-	BOUT:out std_logic_vector(4 downto 0));
+port(clk, RegWrite_in, MemToReg_in: in std_logic;
+	WriteReg_in: std_logic_vector(4 downto 0);
+	ReadData_in, ALURes_in: in std_logic_vector(31 downto 0);
+	RegWrite_out, MemToReg_out: out std_logic;
+	WriteReg_out: out std_logic_vector(4 downto 0);
+	ReadData_out, ALURes_out: out std_logic_vector(31 downto 0));
 end MEMWBPR;
 
 architecture beh of MEMWBPR is
-signal WBCTRLData: std_logic_vector(1 downto 0);
-signal HData, DData: std_logic_vector(32 downto 0);
-signal BData: std_logic_vector(4 downto 0);
+signal RegWrite, MemToReg: std_logic := '0';
+signal WriteReg: std_logic_vector(4 downto 0) := "00000";
+signal ReadData, ALURes: std_logic_vector(31 downto 0)) := X"00000000";
 begin
-	process(CLK)
-	begin
-		if CLK'event and CLK='1' then
-			WBCTRLData <= WBCTRLIN;
-			HData <= HIN;
-			DData <= DIN;
-			BData <= BIN;
-		end if;
-		if CLK'event and CLK='0' then
-			WBCTRLOUT <= WBCTRLData;
-			HOUT <= HData;
-			DOUT <= DData;
-			BOUT <= BData;
-		end if;
-	end process;
+  RegWrite_out <= RegWrite;
+  MemToReg_out <= MemToReg;
+  WriteReg_out <= WriteReg;
+  ReadData_out <= ReadData;
+  ALURes_out <= ALURes;
+process(clk)
+begin
+  if rising_edge(clk) then
+    RegWrite_out <= RegWrite_in;
+    MemToReg_out <= MemToReg_in;
+    WriteReg_out <= WriteReg_in;
+    ReadData_out <= ReadData_in;
+    ALURes_out <= ALURes_in;
+  end if;
+end process;
 end beh;
